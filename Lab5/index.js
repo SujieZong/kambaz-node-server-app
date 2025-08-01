9dfa983d5dcaae517dbf2fcc57e3888f08044535
SujieZong <sjzong1016@outlook.com>
2025-08-01 14:42:32 -0700

    Add CORS middleware and update server functionality
    - Integrated CORS middleware into the application to handle cross-origin requests.
    - Updated index.js to include CORS setup.
    - Enhanced WorkingWithArrays.js with new routes for updating todo completion status and descriptions.
    - Updated package.json to include CORS as a dependency.
    - Updated package-lock.json to reflect the addition of CORS and object-assign packages.

--- Lab5/index.js
+++ Lab5/index.js
@@ -3,6 +3,7 @@ import QueryParameters from "./QueryParameters.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 
+
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
