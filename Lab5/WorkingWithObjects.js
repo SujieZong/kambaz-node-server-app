0f87e3f4cb0ab4ec8d6179a33744159f9ac00291
SujieZong <sjzong1016@outlook.com>
2025-08-01 11:39:49 -0700

    Add WorkingWithArrays and update index.js to include it

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -6,6 +6,12 @@ const assignment = {
   completed: false,
   score: 0,
 };
+const module = {
+    id: 1,
+    name: "CSS and HTML",
+    description: "Learn how to build web applications using CSS and HTML",
+    course: "Web Development",
+};
 export default function WorkingWithObjects(app) {
   app.get("/lab5/assignment", (req, res) => {
     res.json(assignment);
@@ -17,5 +23,34 @@ export default function WorkingWithObjects(app) {
     const { newTitle } = req.params;
     assignment.title = newTitle;
     res.json(assignment);
+  });
+    app.get("/lab5/assignment/score/:newScore", (req, res) => {
+    const { newScore } = req.params;
+    assignment.score = parseInt(newScore);
+    res.json(assignment);
+  });
+
+  app.get("/lab5/assignment/completed/:newCompleted", (req, res) => {
+    const { newCompleted } = req.params;
+    assignment.completed = newCompleted === 'true';
+    res.json(assignment);
+  });
+
+  
+  app.get("/lab5/module", (req, res) => {
+    res.json(module);
+  });
+  app.get("/lab5/module/name", (req, res) => {
+    res.json(module.name);
+  });
+  app.get("/lab5/module/name/:newName", (req, res) => {
+    const { newName } = req.params;
+    module.name = newName;
+    res.json(module);
+  });
+   app.get("/lab5/module/description/:newDescription", (req, res) => {
+    const { newDescription } = req.params;
+    module.description = newDescription;
+    res.json(module);
   });
 }
